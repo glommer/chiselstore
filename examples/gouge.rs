fe2c9b51dd3bfd8e94b78ed35a705d86ff31221b
Glauber Costa <glauber@chiselstrike.com>
2022-08-30 13:47:45 -0400

    gouge from cmd line

--- examples/gouge.rs
+++ examples/gouge.rs
@@ -1,4 +1,5 @@
 use std::io::Write;
+use structopt::StructOpt;
 use tokio::io::{AsyncBufReadExt, BufReader};
 
 pub mod proto {
@@ -8,16 +9,24 @@ pub mod proto {
 use proto::rpc_client::RpcClient;
 use proto::{Consistency, Query};
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "gouge")]
+struct Opt {
+    #[structopt(short, long, default_value = "http://127.0.0.1:50001")]
+    addr: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Opt::from_args();
+
     let stdin = tokio::io::stdin();
     let rdr = BufReader::new(stdin);
     let mut lines = rdr.lines();
     print!("gouge=# ");
     std::io::stdout().flush().unwrap();
     while let Some(line) = lines.next_line().await? {
-        let addr = "http://127.0.0.1:50001";
-        let mut client = RpcClient::connect(addr).await?;
+        let mut client = RpcClient::connect(opt.addr.clone()).await?;
         let query = tonic::Request::new(Query {
             sql: line.to_string(),
             consistency: Consistency::RelaxedReads as i32,
